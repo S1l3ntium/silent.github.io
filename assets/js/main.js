881716e3ceb7bb539cc4a3f944440585c8a33d77
RagenRRR <denis.chichmarev@gmail.com>
2020-07-30 18:47:16 +0300

    beta nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+let menuElem = document.getElementById('navbar__menu');
+let titleElem = menuElem.querySelector('.burger');
+
 function burgerMenu(icon) {
    icon.classList.toggle("change");
+   menuElem.classList.toggle('open');
 }
