bd82a61fca5538a57a6ba58253c3514b95f8de99
RagenRRR <denis.chichmarev@gmail.com>
2020-08-02 04:38:49 +0300

    fix js +animation close nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,6 @@ let titleElem = menuElem.querySelector('.burger');
 
 function burgerMenu(icon) {
    icon.classList.toggle("change");
+   icon.classList.toggle("hover");
    menuElem.classList.toggle('open');
 }
